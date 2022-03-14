a225c655abeb3c87c3ffeba1b6f8b36f1a05907c
MaximoEspitia <max19espitia@gmail.com>
2022-03-14 14:02:16 -0500

    commit tree aa2cd918327303b07094f24c24be3c815c22f544

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age ++) {
+  //  console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+//}
+//const gifts = ["teddy bear", "drone", "doll"];
+//function wrapGifts(gifts) {
+   // for (let i = 0; i < gifts.length; i++) {
+     //   console.log(`Wrapped ${gifts[i]} and added a bow!`);
+       // debugger;
+    //}
+    //return gifts;
+//}
+//wrapGifts(gifts);
+
+function writeCards(arrayOfNames, event) {
+let gratitude = [] 
+for(let i = 0; i < arrayOfNames.length; i++) {
+gratitude.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`);
+  }
+  return gratitude;
+}
+
+function countDown(positiveInteger) {
+  //console.log(positiveInteger)
+  let age = 0
+  while (age <= positiveInteger) {
+    console.log(positiveInteger--)
+  }
+}
